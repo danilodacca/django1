05772daebc804c236ee41406e1f6fb7abacf8eaf
Danilo Dacca <danilopdacca@usp.br>
2023-11-01 11:33:24 -0300

    Criar site estático com Django

--- moviesite/staticpages/views.py
+++ moviesite/staticpages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, 'staticpages/index.html', context)
+
+
+def about(request):
+    context = {}
+    return render(request, 'staticpages/about.html', context)
